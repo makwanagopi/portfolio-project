0da5269e6ec12d55477e16b51cf9811df8337d96
makwanagopi <makgopi007@gmail.com>
2020-04-16 01:09:43 +0530

    debug === true

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +25,7 @@ SECRET_KEY = '4=*s%6p9z(v8hcbgt-v=-7ua&mhi2gx@&l&d)q!79d!vmjqzzn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,5 +136,5 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
