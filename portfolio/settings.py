525020078e68125f6f7b8f6560babd418b541d99
makwanagopi <makgopi007@gmail.com>
2020-04-16 13:01:39 +0530

    database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'portfoliodb',
-        'USER': 'postgres' ,
-        'PASSWORD': 's@a6Azu@' ,
-        'HOST': '52.1.26.16',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
